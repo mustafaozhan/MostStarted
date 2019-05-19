0053a29349e0ad7259f4dd45890090f7f69c3564
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-05-19 15:23:05 +0200

    LanguagesViewModel added

---
+++ MostStarted/Languages/LanguagesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LanguagesViewController.swift
+//  MostStarted
+//
+//  Created by Mustafa Ozhan on 19/05/2019.
+//  Copyright © 2019 Mustafa Ozhan. All rights reserved.
+//
+
+import UIKit
+
+class LanguagesViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
